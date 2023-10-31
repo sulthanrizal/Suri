c6fa4aa6d7ebc8cb05f94712bbd6a88dab4d7db8
sulthanrizal <surie0312@gmail.com>
2023-10-31 14:10:45 +0700

    Revisi

--- Challange17BikinRumus/Run.js
+++ Challange17BikinRumus/Run.js
@@ -1,14 +1,14 @@
 import Calculator, { PI } from "./c17.js"
 
-const cal = new Calculator()
+const calc = new Calculator()
 
-cal.add(10).substract(5).result()// 1 + 10 - 5 = 6
-cal.add(3).multiply(4).divide(6).result()// 6 + 3 * 4 / 6 = 6
-cal.rumus = 7 // set jari jari
-console.log(`nilai sekarang : ${cal.rumus}`)
-cal.multiply(2).multiply(PI).result()// keliling lingkaran dengan jari jari 7 => 2 rumus Pi rumus r = 44
-cal.rumus = 7 // set jari jari
-cal.squere().multiply(PI).result()// luas lingkarang dengan jari jari = 7 = Pi rumus r pangkat 2 = 154
-cal.rumus = 4
-cal.exponent(3).result()// 4 pangkat 3 = 64
-cal.squereRoot().result()// akar pangkat 2 dari 64 = 8
+calc.add(10).substract(5).result()// 1 + 10 - 5 = 6
+calc.add(3).multiply(4).divide(6).result()// 6 + 3 * 4 / 6 = 6
+calc.x = 7 // set jari jari
+console.log(`nilai sekarang : ${calc.x}`)
+calc.multiply(2).multiply(PI).result()// keliling lingkaran dengan jari jari 7 => 2 x Pi x r = 44
+calc.x = 7 // set jari jari
+calc.squere().multiply(PI).result()// luas lingkarang dengan jari jari = 7 = Pi x r pangkat 2 = 154
+calc.x = 4
+calc.exponent(3).result()// 4 pangkat 3 = 64
+calc.squereRoot().result()// akar pangkat 2 dari 64 = 8
--- Challange17BikinRumus/calculator.js
+++ Challange17BikinRumus/calculator.js
@@ -1,39 +1,39 @@
 export const PI = 22 / 7
 
 export default class Calculator {
-    constructor(rumus) {
-        this.rumus = rumus || 1
+    constructor(x) {
+        this.x = x || 1
     }
     add(value) {
-        this.rumus += value
+        this.x += value
         return this
     }
     substract(value) {
-        this.rumus -= value
+        this.x -= value
         return this
     }
     divide(value) {
-        this.rumus /= value
+        this.x /= value
         return this
     }
     multiply(value) {
-        this.rumus *= value
+        this.x *= value
         return this
     }
     squere() {
-        this.rumus = Math.pow(this.rumus, 2)
+        this.x = Math.pow(this.x, 2)
         return this
     }
     exponent(value) { // Bilangan Pangkat
-        this.rumus = Math.pow(this.rumus, value)
+        this.x = Math.pow(this.x, value)
         return this
     }
     squereRoot() { // 
-        this.rumus = Math.sqrt(this.rumus)
+        this.x = Math.sqrt(this.x)
         return this
     }
     result() {
-        console.log(this.rumus)
+        console.log(this.x)
         return this
     }
 }
